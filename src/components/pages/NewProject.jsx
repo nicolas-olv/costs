e4e022d2077672fcf343d96ead620bb572280db4
Nicolas Oliveira <nicolasoliveira.02@outlook.com>
2022-10-20 15:37:43 -0300

    Adição de compoente de formulário

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./NewProject.module.css";
+import ProjectForm from "../project/ProjectForm";
 
 function NewProject() {
-  return <div>NewProject</div>;
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm btnText="Criar projeto" />
+    </div>
+  );
 }
 
 export default NewProject;
